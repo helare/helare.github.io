4b1fa3362611330481a98d9c0193e2ec6e07ae25
Hendrik Laretei <hendrik.laretei@etsnord.ee>
2022-04-05 14:46:18 +0300

    get date dynamically and calculate days between accidents

--- assets/js/xDomainRequest.js
+++ assets/js/xDomainRequest.js
@@ -1,7 +1,7 @@
 
 var xhr = new XMLHttpRequest()
 
-xhr.open("GET", 'https://sheets.googleapis.com/v4/spreadsheets/1chIgh0MSRt3ahHJV-KKqtWMDN5ItPNwEv0jj-XmuRkY/values/Sheet1!c2?key=AIzaSyCP_9mCYNE-d5MBftYGyr0YKjhC9k7DcmI');
+xhr.open("GET", 'https://sheets.googleapis.com/v4/spreadsheets/1chIgh0MSRt3ahHJV-KKqtWMDN5ItPNwEv0jj-XmuRkY/values/Sheet1?key=AIzaSyCP_9mCYNE-d5MBftYGyr0YKjhC9k7DcmI');
 
 xhr.send();
 
@@ -14,9 +14,8 @@ xhr.onreadystatechange = function () {
 }
 
 function addHtml(data) {
-
     var para = document.createElement("div");
-    para.innerHTML = JSON.parse(data).values[0];
+    para.innerHTML = getDays(data);
     para.style.fontWeight = "700";
     para.style.position = "relative";
     para.style.width = "50%";
@@ -25,4 +24,13 @@ function addHtml(data) {
     para.style.margin = "0 auto";
     para.style.lineHeight = "352px";
     document.getElementById("parent").appendChild(para);
+}
+
+function getDays(data) {
+    var lastRow = JSON.parse(data).values.length - 1;
+    var lastAccident = JSON.parse(data).values[lastRow][1].split('-').join('/');
+    var lastAccidentDate = new Date(lastAccident).getTime();
+    var todayDate = new Date().getTime();
+    var diffDays = Math.floor(Math.abs((todayDate - lastAccidentDate) / 86400000));
+    return diffDays;
 }
